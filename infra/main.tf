6c74ea2db3714ba63ebc0f5764712d456be37afc
joe <joe@closet6.com>
2024-06-09 12:30:26 -0400

    s3 backend comment out, default to local storage for now

--- infra/main.tf
+++ infra/main.tf
@@ -5,11 +5,16 @@ terraform {
       version = "~> 5.53"
     }
   }
-  backend "s3" {
-    bucket = ""
-    key    = ""
-    region = ""
-  }
+
+
+  # https://developer.hashicorp.com/terraform/language/settings/backends/s3
+  # backend "s3" {
+  #   bucket = ""
+  #   key    = ""
+  #   region = ""
+  # }
+
+
 
   required_version = ">= 1.8.0"
 }
