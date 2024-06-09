90469c586407203ca7f68ef23b03d179bd406dfb
joe <joe@closet6.com>
2024-06-09 12:08:50 -0400

    adding variables

---
+++ infra/variables.tf
@@ -0,0 +1,28 @@
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the website."
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket without the www. prefix. Normally domain_name."
+}
+
+variable "common_tags" {
+  description = "Common tags you want applied to all components."
+}
+
+variable "r53_zone_id" {
+  type        = string
+  description = "The zone id to apply the record to"
+
+}
+
+variable "env" {
+  type = string
+}
+
+variable "backingkey" {
+  type = string
+
+}
